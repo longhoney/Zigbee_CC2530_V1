896266bca0f1542c28708fc36640b808200d338e
unknown <long.711.sg@gmail.com>
2026-04-04 23:14:24 +0700

    Them dau hieu bat dau truyen

--- examples/Zigbee_SoftwareSerial/Zigbee_SoftwareSerial.ino
+++ examples/Zigbee_SoftwareSerial/Zigbee_SoftwareSerial.ino
@@ -4,13 +4,25 @@
 
 SoftwareSerial mySerial(10, 11); // RX, TX
 
+int timer = 1000;
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   mySerial.begin(19200); //Baudrate của Zigbee CC2530V1
+  delay(500);
+  mySerial.write("Start send");
+  //Serial.println("Start send");
+  for (int i = 1; i < 6; i++){
+    mySerial.write("Hello, world?");
+    //Serial.println("Hello, world?");
+    delay(timer);
+  }
+  mySerial.write("Stop send");
+  //Serial.println("Stop send");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  mySerial.write("Hello, world?");
-  delay(1000);
+
 }
