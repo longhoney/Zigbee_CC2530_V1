0e30ee8b48cc5773cec7570b1842c12f233db98a
unknown <long.711.sg@gmail.com>
2026-04-05 18:36:52 +0700

    TX-RX_WithOutDelay

---
+++ examples/UART_Example_ESP32_copy_20260405182043/UART_Example_ESP32_copy_20260405182043.ino
@@ -0,0 +1,71 @@
+/*
+  Vietduino ESP32 - CC2530v1A - CC2530v1B - USB to UART
+*/
+
+// Define the RX and TX pins for Serial 2
+#define RXD2 16  //D2-IO16
+#define TXD2 17  //D3-IO17
+// Define Baudrate for UART
+#define CC2530_BAUD 19200
+
+const int ledPin = 18;  // the number of the LED pin
+// Variables will change:
+int ledState = LOW;  // ledState used to set the LED
+// The value will quickly become too large for an int to store
+unsigned long previousMillis = 0;  // will store last time LED was updated
+// constants won't change:
+const long interval = 2000;  // interval at which to blink (milliseconds)
+
+// Đặt tên cho chân kết nối cảm biến
+#define analogInPin 34  // IO34-A1 on Vietduino ESP32 (A1 on MakerEdu Shield)
+// Tạo biến số nguyên lưu giá trị cảm biến
+int sensorValue = 0;
+
+// Create an instance of the HardwareSerial class for Serial 2
+HardwareSerial zigbeeSerial(2);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(19200);
+  while (!Serial) {
+    ;  // wait for serial port to connect. Needed for native USB port only
+  }
+  Serial.println("Esp32 start");
+  pinMode(ledPin, OUTPUT);
+  // Start Serial 2 with the defined RX and TX pins and a baud rate of 9600
+  zigbeeSerial.begin(CC2530_BAUD, SERIAL_8N1, RXD2, TXD2);
+  //zigbeeSerial.println("CH340E start to receive");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // Đọc giá trị cảm biến
+  sensorValue = analogRead(analogInPin);
+  //In ra serial monitor
+  //Serial.print("Value: "); Serial.println(sensorValue);
+  
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+
+    // if the LED is off turn it on and vice-versa:
+    if (ledState == LOW) {
+      ledState = HIGH;
+      //Truyen du lieu den Uno
+      if (zigbeeSerial.availableForWrite()){    //Neu chon ham while, ESP32 se gui du lieu lien tuc trong 2 giay
+        zigbeeSerial.print("Value: "); zigbeeSerial.println(sensorValue);
+      }
+      
+    } else {
+      ledState = LOW;
+       if (zigbeeSerial.available() > 0) {
+        char inByte = zigbeeSerial.read();
+        Serial.write(inByte);
+      }
+    }
+    // set the LED with the ledState of the variable:
+    digitalWrite(ledPin, ledState);
+  }
+}
